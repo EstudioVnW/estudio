262266eb14b9223a55c068e651d3554c47d0ff40
romulotunala <romulotunala@gmail.com>
2018-07-13 17:25:19 -0300

    fix somethings

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var slidesPortifolio = 1;
 showPortifolio(slidesPortifolio);
 
+let menu = document.getElementById('menu');
+
 function plusPortifolio(n){
   showPortifolio(slidesPortifolio += n);
-}
+};
 
 function showPortifolio(n){
   var i;
@@ -15,3 +17,49 @@ function showPortifolio(n){
   }
   x[slidesPortifolio-1].style.display = "flex"
 };
+
+// if(window.innerWidth <= '768'){
+
+	function scroll(value){
+
+		document.querySelectorAll('menu-item').addEventListener(
+			'click', function(event){
+			    event.preventDefault();
+		});
+
+		var id = document.getElementById('menu-item').getAttribute('href'),
+			targetOffset = id.getBoundingClientRect().top,
+			menuHeight = document.getElementById('header').innerHeight();
+
+		document.body.scrollTop = targetOffset + menuHeight;	
+	};
+// };
+
+// $('.scroll').click(function(event){
+//     event.preventDefault();
+//     var id = $(this).attr('href'),
+//         targetOffset = $(id).offset().top,
+//         menuHeight = $('.navbar').innerHeight();
+//     $('html, body').animate({
+//         scrollTop: targetOffset - menuHeight
+//     }, 500);
+// });
+
+
+const hiddenMenu = function (){
+	if(window.innerWidth <= '768'){
+		menu.style.display = 'none'	
+	}
+};
+
+hiddenMenu();
+
+function showMenu(){
+	if(menu.style.display === 'none'){
+		menu.style.display = 'flex';
+	}else{
+		menu.style.display = 'none';
+	}
+};
+
+
